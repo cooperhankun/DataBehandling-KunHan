7f14e55890fe8b392423b474e9f9faac9f4c266e
cooperhankun <cooperhanson@gmail.com>
2021-10-24 22:09:01 +0200

    h left

--- Exercises/data_utils.py
+++ Exercises/data_utils.py
@@ -3,7 +3,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def find_missing_values(name_of_the_file):
-    file = pd.read_csv(name_of_the_file)
-    file_missing = file.isnull().sum()
+    file_missing = name_of_the_file.isnull().sum()
     plt.figure(figsize=(20,5))
     return sns.barplot(x=file_missing[file_missing!=0].index, y=file_missing[file_missing!=0].values)
